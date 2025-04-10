b3a78cdb7382350e9a1f49d00a0b7b9d567b147b
Gokulraj Kumarassamy <gkumarassamy@gmail.com>
2025-04-10 01:18:27 -0400

    WE MADE PROFIT FINALLY

--- round1/round_1.py
+++ round1/round_1.py
@@ -12,8 +12,8 @@ class Trader:
         order_depth: OrderDepth = state.order_depths[Products.RAINFOREST_RESIN] 
         orders: list[Order] = []
 
-        upper_bound = 10003.8
-        lower_bound = 9995.9
+        upper_bound = 10002
+        lower_bound = 9997
 
         if len(order_depth.sell_orders) != 0 and len(order_depth.buy_orders) != 0:
             best_ask = min(order_depth.sell_orders.keys())
@@ -21,13 +21,14 @@ class Trader:
             best_bid = max(order_depth.buy_orders.keys())
             best_bid_volume = order_depth.buy_orders[best_bid]
 
-            mid_price = (best_ask + best_bid) / 2
+            print(best_ask, best_bid)
 
-            if mid_price > upper_bound:
+
+            if best_bid >= upper_bound:
                 print("SELL", str(best_bid_volume) + "x", best_bid)
                 orders.append(Order(Products.RAINFOREST_RESIN, best_bid, -best_bid_volume))
 
-            elif mid_price < lower_bound:
+            elif best_ask <= lower_bound:
                 print("BUY", str(-best_ask_volume) + "x", best_ask)
                 orders.append(Order(Products.RAINFOREST_RESIN, best_ask, -best_ask_volume)) 
             else:
